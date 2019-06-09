fcb9b930c970e4e2b11e476c0f92b0f47169bc0c
test1 <test1@xxx.com>
2019-06-09 10:06:56 +0900

    create-rp

---
+++ app/src/testDemo/main1.java
@@ -0,0 +1,22 @@
+package testDemo;
+
+/**
+ *  GIT テスト・デモ用リポジトリ
+ *
+ *
+ *
+ * **/
+
+public class main1 {
+
+	public static void main(String[] args) {
+		// TODO メッセージ出力 ユーザ毎に1行追加
+		// ※他ユーザのメッセージを上書きしないこと
+		System.out.printf("test1\n");
+		System.out.printf("s.inoue");
+		System.out.printf("テスト：新規作成\n");
+		System.out.printf("テスト1：内容の追加（コミット、プッシュ）\n");
+
+	}
+
+}
